e4dde42d1bd9df749de39dbb00ef69a4d46c4e9b
julsoars <j.soarescardosos@gmail.com>
2024-06-06 15:01:25 +0000

    Co-authored-by: Kev 478 <kevgllt@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './App.css'
 function App() {
   return (
     <>
-    App
+    <div className="card">
+      <h2>Rick Sanchez</h2>
+      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+    </div>
     </>
   )
 }
 
 export default App
-
